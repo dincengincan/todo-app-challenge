087a803554bce1edd7027d9569719e1ddb1fd633
dincengincan <engincandinc@gmail.com>
2021-08-25 00:09:55 +0300

    Add todo-list component

---
+++ src/components/todo-list/TodoList.jsx
@@ -0,0 +1,7 @@
+import { StyledTodoList } from './TodoList.styles';
+
+const TodoList = ({ children }) => {
+  return <StyledTodoList>{children}</StyledTodoList>;
+};
+
+export default TodoList;
---
+++ src/components/todo-list/TodoList.styles.jsx
@@ -0,0 +1,5 @@
+import styled from 'styled-components';
+
+export const StyledTodoList = styled.div`
+  margin: 10px 30px;
+`;
