116c1724b6e92625e0d215cf54316449993c3f38
dincengincan <engincandinc@gmail.com>
2021-08-25 02:45:56 +0300

    Add placeholder to textfield

--- src/components/textfield/Textfield.jsx
+++ src/components/textfield/Textfield.jsx
@@ -1,7 +1,14 @@
 import { StyledInput } from './Textfield.styles';
 
-const Textfield = ({ onChange, value }) => {
-  return <StyledInput type="text" value={value} onChange={onChange} />;
+const Textfield = ({ onChange, value, placeholder }) => {
+  return (
+    <StyledInput
+      placeholder={placeholder}
+      type="text"
+      value={value}
+      onChange={onChange}
+    />
+  );
 };
 
 export default Textfield;
