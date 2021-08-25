f3e443a24bff5ce4db2a130e80ae2d435dca2c0c
dincengincan <engincandinc@gmail.com>
2021-08-25 05:01:49 +0300

    Add deadline option

--- src/components/add-todo/AddTodo.jsx
+++ src/components/add-todo/AddTodo.jsx
@@ -1,44 +1,33 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 
-import { Container, StyledForm } from './AddTodo.styles';
+import { useSelector } from 'react-redux';
+
 import Textfield from '../textfield/Textfield';
 import Button from '../button/Button';
 import { BUTTON_VARIANTS } from '../button/Button';
 import Checkbox from '../checkbox/Checkbox';
-import { FETCH_URL } from '../../constants/general';
 import Row from '../row/Row';
+import Datefield from '../datefield/Datefield';
 
-const AddTodo = ({ addTodo }) => {
+const AddTodo = ({ addTodo, onReset }) => {
+  const todos = useSelector((state) => state.todos);
+
   const [todoInput, setTodoInput] = useState('');
-  const [deadline, setDeadline] = useState(false);
+  const [deadline, setDeadline] = useState('');
+  const [showDeadline, setShowDeadline] = useState(false);
 
   const handleTodoInput = (e) => {
     setTodoInput(e.target.value);
   };
 
-  const handleDeadlineChange = (hasDeadline) => {
-    setDeadline(hasDeadline);
-  };
-
   const handleFormSubmit = async (e) => {
     e.preventDefault();
-    addTodo(todoInput);
-  };
-
-  const handleReset = async () => {
-    await fetch(FETCH_URL, {
-      method: 'DELETE',
-      body: JSON.stringify({
-        label: todoInput,
-      }),
-      headers: {
-        'Content-type': 'application/json; charset=UTF-8',
-      },
-    });
+    setTodoInput('');
+    addTodo(todoInput, deadline);
   };
 
   return (
-    <>
+    <Row style={{ display: 'flex', flexDirection: 'column' }}>
       <Row>
         <form onSubmit={handleFormSubmit}>
           <Textfield
@@ -46,22 +35,35 @@ const AddTodo = ({ addTodo }) => {
             value={todoInput}
             onChange={handleTodoInput}
           />
-          <Button type="submit" text="Add" variant={BUTTON_VARIANTS.PRIMARY} />
           <Button
-            onClick={() => handleReset}
+            disabled={!todoInput}
+            type="submit"
+            text="Add"
+            variant={BUTTON_VARIANTS.PRIMARY}
+          />
+          <Button
+            disabled={!todos.length}
+            onClick={onReset}
             text="Remove"
             variant={BUTTON_VARIANTS.PRIMARY}
           />
         </form>
       </Row>
-      <Row>
+
+      <Row style={{ display: 'flex', alignItems: 'center', height: '20px' }}>
         <Checkbox
-          label="Set deadline"
-          checked={deadline}
-          onChange={handleDeadlineChange}
+          label="Set Deadline"
+          checked={showDeadline}
+          onChange={setShowDeadline}
         />
+        {showDeadline && (
+          <Datefield
+            value={deadline}
+            onChange={(e) => setDeadline(e.target.value)}
+          />
+        )}
       </Row>
-    </>
+    </Row>
   );
 };
 
